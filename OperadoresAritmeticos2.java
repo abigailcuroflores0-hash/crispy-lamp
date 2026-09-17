8c2ff076f7077882399e5210a5016ad8f71977e7
abigailcuroflores0-hash <abigailcuroflores0@gmail.com>
2026-09-17 00:35:58 -0500

    Add files via upload

---
+++ OperadoresAritmeticos2.java
@@ -0,0 +1,16 @@
+public class OperadoresAritmeticos2 {
+
+    public static void main(String[] args) {
+
+        double numero1 = 7.0;
+        double numero2 = 5.0;
+
+        System.out.println("Operaciones con\n\"numero1\" y \"numero2\":");
+        System.out.println(numero1 + numero2);
+        System.out.println(numero1 - numero2);
+        System.out.println(numero1 * numero2);
+        System.out.println(numero1 / numero2);
+        System.out.println(numero1 % numero2);
+
+    } // Fin del método main
+} // Fin de la clase OperadoresAritmeticos2
